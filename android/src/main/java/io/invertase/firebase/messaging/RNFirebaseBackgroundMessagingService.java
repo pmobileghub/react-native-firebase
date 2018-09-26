cb8e55cb572a3b3fe74972a7f3d4e5a69df9bea3
Yaniv Tabibi <ytabibi@productivemobile.com>
2018-09-26 16:06:12 +0200

    Allow notifications to not crash the app

--- android/src/main/java/io/invertase/firebase/messaging/RNFirebaseBackgroundMessagingService.java
+++ android/src/main/java/io/invertase/firebase/messaging/RNFirebaseBackgroundMessagingService.java
@@ -22,7 +22,7 @@ public class RNFirebaseBackgroundMessagingService extends HeadlessJsTaskService 
         "RNFirebaseBackgroundMessage",
         messageMap,
         60000,
-        false
+        true
       );
     }
     return null;
